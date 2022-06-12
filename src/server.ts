98dd830fc63db755089261ad6510f901228e37ff
kraccoon-dev <kraccoon@kakao.com>
2022-06-12 13:07:40 +0900

    fix: port

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,54 @@
-import express, { Request, Response } from 'express'
-import { PrismaClient } from '@prisma/client'
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
-const app = express()
-const prisma = new PrismaClient()
+const app = express();
+const prisma = new PrismaClient();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (_, res: Response) => {
-  res.send('hi there')
-})
+  res.send('hi there');
+});
 
 app.post('/add', async (req: Request, res: Response) => {
-  console.log(req.config)
+  console.log(req.config);
   const body = {
     email: req.body.email,
     name: req.body.name,
-  }
+  };
 
   try {
     const user = await prisma.user.create({
       data: { ...body },
-    })
+    });
 
-    res.json(user)
+    res.json(user);
   } catch (error) {
-    console.log(error)
-    res.status(500).send('fail to create user')
+    console.log(error);
+    res.status(500).send('fail to create user');
   }
-})
+});
 
 app.get(
   '/get/:email',
   async (req: Request<{ email: string }>, res: Response) => {
-    const email = req.params.email
+    const email = req.params.email;
 
     try {
       const user = await prisma.user.findUnique({
         where: {
           email,
         },
-      })
+      });
 
-      res.json(user)
+      res.json(user);
     } catch (error) {
-      console.log(error)
-      res.status(500).send('fail to find user')
+      console.log(error);
+      res.status(500).send('fail to find user');
     }
   }
-)
+);
 
-app.listen(3000, () => console.log("server's listening on port 3000"))
+app.listen(process.env['PORT'] || 3000, () =>
+  console.log("server's listening on port 3000")
+);
